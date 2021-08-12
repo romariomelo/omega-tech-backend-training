2dbc294b3a56167066d98c31e6e4ad4d07ff49ea
romario <romario.melos@gmail.com>
2021-08-12 09:52:30 -0300

    Implements setting of enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConfigModule } from './config/config.module';
 import { ContractModules } from './contracts/contract.module';
 import { DatabaseModule } from './database/databse.module';
 
 @Module({
-  imports: [DatabaseModule, ContractModules],
+  imports: [AppConfigModule, DatabaseModule, ContractModules],
   controllers: [AppController],
   providers: [AppService],
 })
